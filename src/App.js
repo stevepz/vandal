0bc1d1d7f506cdad663ad84148e5f40e8452f145
slvteblvvk <43097045+slvteblvvk@users.noreply.github.com>
2019-11-15 16:27:46 -0500

    vandal update 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Route, Link, withRouter } from 'react-router-dom';
+import Header from './components/Header';
+import Welcome from './components/Welcome';
+import Resteraunt from './components/Resteraunt';
+import Nightclub from './components/Nightclub';
 
-class App extends React.Component() {
+
+class App extends React.Component {
 
 
 
